a906e32fd6f92b88a44d1dffcd27c06537136959
sfah007 <69042482+sfah007@users.noreply.github.com>
2021-02-06 12:54:03 +0300

    Update a.php

--- a.php
+++ a.php
@@ -1,19 +1,21 @@
 <?php  
 
-ob_start(); 
-$API_KEY = "1124323028:AAFp6uHmMMVn9aovrL4Kq1n-ThGAav7cVuk";#توكن البوت 
-define('API_KEY',$API_KEY); 
-echo "<a href='https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']."'>setwebhook</a>"; 
-echo file_get_contents("https://api.telegram.org/bot$API_KEY/setwebhook?url=".$_SERVER['SERVER_NAME']."".$_SERVER['SCRIPT_NAME']); 
-function bot($method,$datas=[]){ 
-$url = "https://api.telegram.org/bot".API_KEY."/".$method; 
-$ch = curl_init(); 
-curl_setopt($ch,CURLOPT_URL,$url); curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); 
-curl_setopt($ch,CURLOPT_POSTFIELDS,$datas); 
-$res = curl_exec($ch); 
-if(curl_error($ch)){ 
-var_dump(curl_error($ch)); 
-}else{return json_decode($res);}} 
+<?php
+$API_KEY = "1124323028:AAFp6uHmMMVn9aovrL4Kq1n-ThGAav7cVuk";
+define('api',$API_KEY); 
+function bot($method,$datas=[]){
+    $url = "https://api.telegram.org/bot".api."/".$method;
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_POSTFIELDS,http_build_query($datas));
+    $res = curl_exec($ch);
+    if(curl_error($ch)){
+        var_dump(curl_error($ch));
+    }else{
+        return json_decode($res);
+    }
+}
 $update   = json_decode(file_get_contents('php://input'));
 $message    = $update->message;
 $text       = $message->text;
